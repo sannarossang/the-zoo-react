2af8c400fbf94661d699e9d37ecb179001743e24
sannarossang <sanna.rossang@gmail.com>
2023-05-08 07:15:51 +0200

    (refactor): removed separate LS-function

--- src/helpers/localStorage.ts
+++
@@ -1,5 +0,0 @@
-import { IAnimal } from "../models/IAnimal";
-
-export function saveAnimalsToLS(currentAnimals: IAnimal) {
-  localStorage.setItem("currentAnimals", JSON.stringify(currentAnimals));
-}
